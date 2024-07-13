f46e1eaa1e7566b6bf8f6e19c18d9d2ca872a3e4
dany-muriel-herencia <drmurielh@unjbg.edu.pe>
2024-07-13 15:51:13 -0500

    agrege la edad mayor

--- ejercicio 2.cpp
+++ ejercicio 2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b,n;
+    int a=0,b,n;
     cout<<"ingrese la cantidad de los personas : ";cin>>n;
          struct persona{
     char nombre[20];
@@ -16,6 +16,20 @@ int main(){
         cout<<"ingrese el nombre del usuario  "<<i+1<<" :  ";cin.ignore();cin.getline(personas[i].nombre,20);
         cout<<"ingrese el numero de dni del usuario  "<<i+1<<" :  ";cin.ignore();cin.getline(personas[i].dni,89);
         cout<<"ingrese la edad del usuario  "<<i+1<<" :  ";cin>>personas[i].edad;
+
+        //implementacioj de la if para hallar promedios
+
+        a=personas[i].edad+a;
+        
+        }
+        cout<<"usuarios con edad mayor:   \n";
+    
+    for(int k;k<n;k++){
+        if(personas[k].edad>50){
+            cout<<personas[k].nombre,20;
+            cout<<endl;
+
+        }
     }
     return 0;
 }
