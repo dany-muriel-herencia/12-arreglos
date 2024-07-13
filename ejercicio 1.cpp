0a5bbffc145e5f1ebb940c4b364c6cb1f834b07a
dany-muriel-herencia <drmurielh@unjbg.edu.pe>
2024-07-12 21:48:34 -0500

    ejercicio 1 acabado

--- ejercicio 1.cpp
+++ ejercicio 1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 int main(){
-    int a,b,n,c;
+    int a,b,n,ma=000,me=67946889;
     cout<<"ingrese la cantidad de los empleados: ";cin>>n;
          struct personal{
     char nombre[20];
@@ -18,9 +18,24 @@ int main(){
         cin.ignore();cin.getline(empleados[i].sexo,89);
         cout <<"digite el sueldo : ";
         cin>>empleados[i].sueldo;
-        
+
+        if(empleados[i].sueldo < me){
+            me = empleados[i].sueldo;
+            a=i;
+        }
+        if(empleados[i].sueldo > ma){
+            ma=empleados[i].sueldo;
+            b=i;
+
+        }
     }
-    
+    cout << "salario mayor \n ";
+    cout << "Nombre: " << empleados[a].nombre << "\n";
+    cout << "Con un sueldo de: " << empleados[a].sueldo << "\n";
+
+    cout << "salario menor\n";
+    cout << "Nombre: " << empleados[b].nombre << "\n";
+    cout << "Con un sueldo de: " << empleados[b].sueldo << "\n";
 
 
 
