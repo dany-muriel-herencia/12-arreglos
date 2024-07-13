eff476847e6b6a698edb03ae9537722c9ec89a64
dany-muriel-herencia <drmurielh@unjbg.edu.pe>
2024-07-12 21:28:42 -0500

    es el primer avance

--- ejercicio 1.cpp
+++ ejercicio 1.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
 using namespace std;
 
-struct personal{
-    char nombre[20];
-    char apellido[89];
-    int edad[]
 
-};
+int main(){
+    int a,b,n,c;
+    cout<<"ingrese la cantidad de los empleados: ";cin>>n;
+         struct personal{
+    char nombre[20];
+    char sexo[89];
+    int sueldo;
+
+    }empleados[n];
+    for(int i=0;i<n;i++){
+        cout<< "digite el nombre : ";
+        cin.ignore();cin.getline(empleados[i].nombre,20);
+        cout<<"digite el sexo : ";
+        cin.ignore();cin.getline(empleados[i].sexo,89);
+        cout <<"digite el sueldo : ";
+        cin>>empleados[i].sueldo;
+        
+    }
+    
+
+
+
+    return 0;
+}
