a748681ecddab33e9068bfde1cd549f9627fa9cc
dany-muriel-herencia <drmurielh@unjbg.edu.pe>
2024-07-14 09:25:46 -0500

    acabamos la tarea}

--- ejercicio 4.cpp
+++ ejercicio 4.cpp
@@ -2,31 +2,70 @@
 . Desarrolle un programa que lea en un arreglo de registros los datos de n atletas y luego lea el nombre 
 de un país. El programa deberá mostrar los atletas registrados que provienen de ese país y debe indicar
  cuál es el atleta con mayor número de medallas de ese país.*/
- #include<iostream>
- using namespace std;
-struct olimpicos{
+#include <iostream>
+#include <cstring> // Para utilizar strcmp
+using namespace std;
+
+struct Olimpico {
     char nombres[342];
     char pais[99];
-    char disiplina[99];
+    char disciplina[99];
     int medallas;
 };
 
-int main(){
-    int n,d;
+int main() {
+    int n;
 
-    cout<<" ingrese la cantidad de atletas : ";cin>>n;
+    cout << "Ingrese la cantidad de atletas: ";
+    cin >> n;
 
-    olimpicos personas[n];
+    Olimpico personas[n];
 
-    for (int i = 0; i < n; i++){
-
-    
-
-    cout<<"ingrese el nombre del usuario  "<<i+1<<" :  ";cin.ignore();cin.getline(personas[i].nombres,340);
-    cout<<"ingrese el numero de dni del usuario  "<<i+1<<" :  ";cin.ignore();cin.getline(personas[i].pais,99);
-    cout<<"ingrese el numero de dni del usuario  "<<i+1<<" :  ";cin.ignore();cin.getline(personas[i].disiplina,99);
-    cout<<"ingrese la edad del usuario  "<<i+1<<" :  ";cin>>personas[i].medallas;
-
+    for (int i = 0; i < n; i++) {
+        cout << "Ingrese el nombre del atleta " << i + 1 << ": ";
+        cin.ignore();
+        cin.getline(personas[i].nombres, 342);
+        cout << "Ingrese el país del atleta " << i + 1 << ": ";
+        cin.getline(personas[i].pais, 99);
+        cout << "Ingrese la disciplina del atleta " << i + 1 << ": ";
+        cin.getline(personas[i].disciplina, 99);
+        cout << "Ingrese el número de medallas del atleta " << i + 1 << ": ";
+        cin >> personas[i].medallas;
     }
+
+    char paisConsulta[99];
+    cout << "Ingrese el nombre del país a consultar: ";
+    cin.ignore();
+    cin.getline(paisConsulta, 99);
+
+    int maxMedallas = -1;
+    Olimpico* atletaMaxMedallas = nullptr;
+
+    cout << "Atletas de " << paisConsulta << ":\n";
+    bool hayAtletas = false;
+    for (int i = 0; i < n; i++) {
+        if (strcmp(personas[i].pais, paisConsulta) == 0) {
+            hayAtletas = true;
+            cout << "Nombre: " << personas[i].nombres << endl;
+            cout << "Disciplina: " << personas[i].disciplina << endl;
+            cout << "Número de Medallas: " << personas[i].medallas << endl;
+            cout << "--------------------------" << endl;
+
+            if (personas[i].medallas > maxMedallas) {
+                maxMedallas = personas[i].medallas;
+                atletaMaxMedallas = &personas[i];
+            }
+        }
+    }
+
+    if (!hayAtletas) {
+        cout << "No se encontraron atletas de " << paisConsulta << ".\n";
+    } else if (atletaMaxMedallas != nullptr) {
+        cout << "El atleta con más medallas de " << paisConsulta << " es:\n";
+        cout << "Nombre: " << atletaMaxMedallas->nombres << endl;
+        cout << "Disciplina: " << atletaMaxMedallas->disciplina << endl;
+        cout << "Número de Medallas: " << atletaMaxMedallas->medallas << endl;
+    }
+
     return 0;
 }
